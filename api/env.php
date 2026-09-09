c0cde4e33b9941d7fbf4012e53722f44d09ea3f2
ebita <k.ebita@tplus2017.jp>
2026-09-09 11:21:12 +0900

    SEO・AIO対策としてタイトルや説明文を変更し、Google AnalyticsおよびClarityのトラッキングタグを追加

--- api/env.php
+++ api/env.php
@@ -1,3 +1,3 @@
 <?php
 // 本番環境にアップロードする際は、ここの値を実際のGoogle Client IDに書き換えてください。
-define('GOOGLE_CLIENT_ID', 'YOUR_GOOGLE_CLIENT_ID_HERE');
+define('GOOGLE_CLIENT_ID', '732286662349-kpd4qclqq8u3eqlahaoaaj1pba40a8jf.apps.googleusercontent.com');
