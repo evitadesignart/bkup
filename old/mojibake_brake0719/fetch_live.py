035563c46c03402da4c461d81e1c0d7b97a01fa9
ebita <k.ebita@tplus2017.jp>
2026-09-17 08:52:31 +0900

    不要ファイル(oldディレクトリ)を削除

--- old/mojibake_brake0719/fetch_live.py
+++
@@ -1,9 +0,0 @@
-﻿import urllib.request
-try:
-    req = urllib.request.urlopen("https://evitadesignart.com/tarotte.php")
-    html = req.read().decode('utf-8')
-    lines = html.split('\n')
-    for i in range(25, min(35, len(lines))):
-        print(f"{i}: {lines[i]}")
-except Exception as e:
-    print(e)
